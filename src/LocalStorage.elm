25d6e0813ee0d5cc17ef9334c54452a6d829c18a
Anton Ovechkin <owanturist@gmail.com>
2017-08-27 22:08:58 +0700

    Implement full localStorage API

--- src/LocalStorage.elm
+++ src/LocalStorage.elm
@@ -1,18 +1,55 @@
-module LocalStorage exposing (length)
+module LocalStorage exposing (clear, getItem, key, length, removeItem, setItem)
 
 {-| Your application often want to storage data in localStorage.
 This library helps your manage of localStorage.
 
-@docs length
+@docs length, key, getItem, setItem, removeItem, clear
 
 -}
 
+import Json.Decode as Decode
+import Json.Encode as Encode
 import Native.LocalStorage
 import Task exposing (Task)
 
 
-{-| Get the current localStorage length.
+{-| Returns an integer representing the number of data items stored in the localStorage object.
 -}
 length : Task x Int
 length =
     Native.LocalStorage.length
+
+
+{-| When passed a number n, this method will return the name of the nth key in the storage.
+-}
+key : Int -> Task x (Maybe String)
+key =
+    Native.LocalStorage.key
+
+
+{-| When passed a key name, will return that key's value.
+-}
+getItem : String -> Task String (Maybe Decode.Value)
+getItem =
+    Native.LocalStorage.getItem
+
+
+{-| When passed a key name and value, will add that key to the storage, or update that key's value if it already exists.
+-}
+setItem : String -> Encode.Value -> Task x ()
+setItem =
+    Native.LocalStorage.setItem
+
+
+{-| When passed a key name, will remove that key from the storage.
+-}
+removeItem : String -> Task x ()
+removeItem =
+    Native.LocalStorage.removeItem
+
+
+{-| When invoked, will empty all keys out of the storage.
+-}
+clear : Task x ()
+clear =
+    Native.LocalStorage.clear
--- src/Native/LocalStorage.js
+++ src/Native/LocalStorage.js
@@ -1,13 +1,78 @@
-var _invigos$www_webapp_dev$Native_LocalStorage = (function LocalStorage(Scheduler) {
-var length = Scheduler.nativeBinding(function length(callback) {
+var _invigos$www_webapp_dev$Native_LocalStorage = (function LocalStorage(
+    Scheduler,
+    Just,
+    Nothing,
+    Utils
+) {
+var length = Scheduler.nativeBinding(function bindedLength(callback) {
     callback(
         Scheduler.succeed(localStorage.length)
     );
 });
 
+var key = function key(n) {
+    return Scheduler.nativeBinding(function bindedKey(callback) {
+        var keyStr = localStorage.key(n);
+
+        callback(
+            Scheduler.succeed(
+                keyStr === null ? Nothing : Just(keyStr)
+            )
+        );
+    });
+};
+
+var getItem = function getItem(key) {
+    return Scheduler.nativeBinding(function bindedGetItem(callback) {
+        var value = localStorage.getItem(key);
+
+        if (value === null) {
+            Scheduler.succeed(Nothing);
+        } else {
+            try {
+                Scheduler.succeed(
+                    JSON.parse(value)
+                );
+            } catch (err) {
+                Scheduler.fail(err)
+            }
+        }
+    });
+};
+
+var setItem = function setItem(key, value) {
+    return Scheduler.nativeBinding(function bindedSetItem(callback) {
+        localStorage.setItem(key, JSON.stringify(value));
+
+        Scheduler.succeed(Utils.Tuple0);
+    });
+};
+
+var removeItem = function removeItem(key) {
+    return Scheduler.nativeBinding(function bindedRemoveItem(callback) {
+        localStorage.removeItem(key);
+
+        Scheduler.succeed(Utils.Tuple0);
+    });
+};
+
+var clear = Scheduler.nativeBinding(function bindedRemoveItem(callback) {
+    localStorage.clear();
+
+    Scheduler.succeed(Utils.Tuple0);
+});
+
 return {
-    length: length
+    length: length,
+    key: key,
+    getItem: getItem,
+    setItem: setItem,
+    removeItem: removeItem,
+    clear: clear
 };
 })(
-    _elm_lang$core$Native_Scheduler
+    _elm_lang$core$Native_Scheduler,
+    _elm_lang$core$Maybe$Just,
+    _elm_lang$core$Maybe$Nothing,
+    _elm_lang$core$Native_Utils
 );
